f73ca3817b52c64d51a6de1f2b96f47e68794bb4
Lucile Gentner <lucile.gentner@gmail.com>
2018-05-14 16:29:50 +0200

    Add comment form in thread page

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,10 +3,12 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Thread;
+use AppBundle\Entity\Comment;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Form\Type\ThreadType;
+use AppBundle\Form\Type\CommentType;
 
 class DefaultController extends Controller
 {
@@ -88,5 +90,47 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
             'form' => $form->createView()
         ]);
+    }
+
+    /**
+     * @Route("/thread/{id}/comment", name="add_comment")
+     */
+    public function addCommentAction($id, Request $request)
+    {
+        $comment = new Comment();
+
+        $action = $this->generateUrl('add_comment', array('id' => $id));
+
+        $form = $this->createForm(CommentType::class, $comment, array('action' => $action));
+
+        if ($request->isMethod('POST')) {
+
+            $form->handleRequest($request);
+
+            if ($form->isValid()) {
+
+                $thread = $this->getDoctrine()
+                    ->getRepository('AppBundle:Thread')
+                    ->find($id);
+
+                $comment->setDate(new \DateTime());
+                $comment->setUserId(1);
+                $comment->setThread($thread);
+
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($comment);
+                $em->flush();
+
+            }
+
+            return $this->redirect($this->generateUrl('thread', array('id' => $id)));
+        }
+
+        return $this->render('partials/add_comment.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'form' => $form->createView()
+        ]);
+
     }
 }
